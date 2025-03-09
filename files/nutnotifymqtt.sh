2e15c07b9c104dfc58f28be354048cc70cc1ef35
ensjens <64878876+ensjens@users.noreply.github.com>
2025-03-08 19:02:18 -0500

    Add mqtt notifications

--- files/nutnotifymqtt.sh
+++ files/nutnotifymqtt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mosquitto_pub -h mqtt.jmcal.us -p 1883 -t "power/$UPSNAME/notify/$NOTIFYTYPE" -m "$1" -u nut -P jdgPoeh2MK6yjj2kF6U7
+mosquitto_pub -h 10.0.80.10 -p 1883 -t "power/$UPSNAME/notify/$NOTIFYTYPE" -m "$1" -u nut -P {{mqtt_nut_password}}
